556213dae79ce7c451943638c885c15b6fe44970
Tomomi Muta <tomomimuta@icloud.com>
2025-04-22 21:44:17 +0900

    mysql

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,14 +1,20 @@
 package raisetech.student.management;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
+
+	@Autowired
+	private StudentRepository repository;
+
 
 	private String name = "Muta Tomomi";
   private String age ="27";
@@ -17,17 +23,16 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-@GetMapping("/studentInfo")
-public String studentInfo() {
-
-	return name + " " + age + "歳";
+@GetMapping("/student")
+public String getStudent(@RequestParam String name) {
+	 Student student = repository.searchByName(name);
+	return student.getName() + " " + student.getAge()+ "歳";
  }
 
 
- @PostMapping("/studentInfo")
-	public void setStudentInfo(String name, String age){
-		this.name = name;
-		this.age = age;
+ @PostMapping("/student")
+	public void registerStudent(String name, int age){
+		repository.registerStudent(name,age);
  }
 
  @PostMapping("/studentName")
--- src/main/java/raisetech/student/management/StudentRepository.java
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -1,5 +1,18 @@
 package raisetech.student.management;
 
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
 public interface StudentRepository {
+
+  @Select("SELECT * FROM student WHERE name = #{name}")
+  Student searchByName(String name);
+
+  @Insert("INSERT student values(#{name},#{age})")
+  void registerStudent(String name, int age);
+
 
 }
